3f80a78b8d7c5fb72b9c0eeae772b8323d57a517
MuhammadAman02 <mohammada@hst.ie>
2025-07-10 18:40:03 +0500

    update file

---
+++ src/routes/fruit.route.ts
@@ -0,0 +1,17 @@
+import { FastifyInstance } from 'fastify';
+import { getFruitsHandler, getFruitByIdHandler } from '../controllers/fruit.controller';
+import { getFruitsSchema, getFruitByIdSchema } from '../schemas/fruit.schema';
+
+export async function fruitRoutes(app: FastifyInstance) {
+  // GET /api/fruits - Get all fruits
+  app.get('/api/fruits', {
+    schema: getFruitsSchema,
+    handler: getFruitsHandler,
+  });
+
+  // GET /api/fruits/:id - Get fruit by ID
+  app.get('/api/fruits/:id', {
+    schema: getFruitByIdSchema,
+    handler: getFruitByIdHandler,
+  });
+}
