4428f574960985f70c7f1062d7ce46bc462b7a7e
MuhammadAman02 <mohammada@hst.ie>
2025-07-10 18:50:20 +0500

    update file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig } from "drizzle-kit";
+import type { Config } from 'drizzle-kit';
+import { env } from './src/config/env';
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
-}
-
-export default defineConfig({
-  out: "./migrations",
-  dialect: "postgresql",
-  schema: "./src/db/schema.ts",
+export default {
+  schema: './src/db/schema.ts',
+  out: './drizzle',
+  driver: 'pg',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    connectionString: env.DATABASE_URL,
   },
-});
+} satisfies Config;
