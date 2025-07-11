a900c08829025df7f5990efbeaba398c9f3eb7d2
MuhammadAman02 <mohammada@hst.ie>
2025-07-11 10:56:35 +0500

    update file

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,3 +8,12 @@ export const fruits = pgTable('fruits', {
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
+
+export const vegetables = pgTable('vegetables', {
+  id: serial('id').primaryKey(),
+  name: text('name').notNull(),
+  color: text('color').notNull(),
+  category: text('category').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
